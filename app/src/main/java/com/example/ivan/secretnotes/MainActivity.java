1163a9b0cefc28c1c00061f929ac44536d821984
ijurasic <ivan.jurasic@gmail.com>
2016-07-26 10:10:09 +0200

    bckup

--- app/src/main/java/com/example/ivan/secretnotes/MainActivity.java
+++ app/src/main/java/com/example/ivan/secretnotes/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ivan.secretnotes;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import dbutils.DButil;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("POSITION", "MainActivity.onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DButil dbutil = new DButil(this);
--- app/src/main/java/dbutils/DButil.java
+++ app/src/main/java/dbutils/DButil.java
@@ -3,6 +3,7 @@ package dbutils;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.example.ivan.secretnotes.MainActivity;
 
@@ -15,16 +16,22 @@ public class DButil  extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table notes(id integer primary key autoincrement, note text)");
+        Log.d("POSITION", "DButil.onCreate");
+        sqLiteDatabase.execSQL("create table users(id integer NOT NULL primary key autoincrement, username text NOT NULL, password text NOT NULL)");
+        sqLiteDatabase.execSQL("create table notes(id integer NOT NULL primary key autoincrement,user_id integer NOT NULL, note text NOT NULL," +
+                "FOREIGN KEY(user_id) REFERENCES users(id))");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        Log.d("POSITION", "DButil.onUpgrade");
 
     }
 
     public DButil(Context context) {
+
         super(context, DB_NAME, null, 1);
+        Log.d("POSITION", "DButil.DButil");
         dbSecretNotes=this.getWritableDatabase();
         }
 
